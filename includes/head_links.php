6821779d3deedd9206e349b00d4d9420b302b247
Shubham <Shubham763288@gmail.com>
2024-01-15 13:35:32 +0530

    first commit

---
+++ includes/head_links.php
@@ -0,0 +1,6 @@
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap" rel="stylesheet">
+<link href="css/common.css" rel="stylesheet" />
